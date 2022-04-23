b68b566bc2f6ab7154d2b30dd4d7d74229cca004
aazawarfare <66241185+azaregon@users.noreply.github.com>
2022-04-23 15:03:13 +0700

    Update script.js

--- SimpleQ/script.js
+++ SimpleQ/script.js
@@ -8,6 +8,7 @@ var totalAyah_now = 0;
 
 function QuranTextHandling(data) {
   //console.log(text);
+  alert("TextHandling is called");
   var dataArabic = data['data'][0];
   var surah_arabic = dataArabic['text'];
   var surah_name = dataArabic['surah']['englishName'];
